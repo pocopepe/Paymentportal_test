eec9434d5bdbf672396d00215d70ed1060336545
viju <avijusanjai@gmail.com>
2024-07-31 09:26:41 +0530

    Account Schema Added

--- Backend/schemas.js
+++ Backend/schemas.js
@@ -1,9 +1,23 @@
 const connectDB=require('./db');
 connectDB();
 
-const User = mongoose.model('User', { email: String, password: String, firstname: String, lastname: String});
+const userSchema=mongoose.Schema({ email: String, password: String, firstname: String, lastname: String})
+const User = mongoose.model('User', userSchema);
+
+const accountSchema= new mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    balence: {
+        type: Number, 
+        required: true,
+    }
+})
+const Account= mongoose.model('Accounts', accountSchema)
 
 
 
 
-module.exports = {User};
+module.exports = {User, Account};
